a8a355390a32ec4d90f024712688bfa48f6b4133
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-02-27 20:50:51 -0600

    finished database.js, install.js, and package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,59 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+/*https://www.npmjs.com/package/idb
+
+Promises & throwing
+The library turns all IDBRequest objects into promises, but it doesn't know in advance which methods may return promises.
+As a result, methods such as store.put may throw instead of returning a promise.
+If you're using async functions, there's no observable difference.
+
+//const db = await openDB(name, version)
+//name: Name of the database.
+version (optional): Schema version, or undefined to open the current version.
+
+const tx = db.transaction('keyval', 'readwrite');
+const store = tx.objectStore('keyval');
+const request = store.put({ id: id, todo: content });
+*/
+
+//PUT funciton
+export const putDb = async (id, content) => {
+  console.log('PUT to the jateDB');
+  // connect to DB and version we want to use
+  const jateDb = await openDB('jate', 1);
+  // tx.store - If a transaction involves a single store, the store property will reference that store.
+  // since we have one store name, is 'readwrite neccesary? (question to ask)
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // open the object to store
+  /*
+  If a transaction involves multiple stores, tx.store is undefined, you need to use tx.objectStore(storeName) 
+  to get the stores.
+  */
+  const store = tx.objectStore('jate')
+  //
+  const request = store.put({ id: id, value: content });
+  //
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+/*
+export async function get(key) {
+  return (await dbPromise).get('keyval', key);
+}
+*/
+export const getDb = async () => {
+  console.log('GET all from the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,65 @@
 const butInstall = document.getElementById('buttonInstall');
 
+/*
+https://web.dev/codelab-make-installable/
+Listen for the beforeinstallprompt event #
+When the browser fires the beforeinstallprompt event, that's the indication that the Progressive Web App can be installed and an install button can be shown to the user. The beforeinstallprompt event is fired when the PWA meets the installability criteria.
+
+Click Remix to Edit to make the project editable.
+Add a beforeinstallprompt event handler to the window object.
+Save the event as a global variable; we'll need it later to show the prompt.
+Unhide the install button.
+*/
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+   // Prevent the mini-infobar from appearing on mobile.
+   event.preventDefault();
+   console.log('👍', 'beforeinstallprompt', event); 
+   // Stash the event so it can be triggered later.
+   window.deferredPrompt = event;
+   // Remove the 'hidden' class from the install button.
+   butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+/*
+ To show the install prompt, call prompt() on the
+  saved beforeinstallprompt event. 
+  Calling prompt() is done in the install button 
+  click handler because prompt() must be called 
+  from a user gesture.
+*/
+
+butInstall.addEventListener('click', async () => {
+ console.log('👍', 'butInstall-clicked');
+ const promptEvent = window.deferredPrompt;
+ if(!promptEvent){
+  // The deferred prompt isn't available.
+    return;
+ }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+  // Hide the install button.
+  butInstall.classList.toggle('hidden', true);
+});
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+/*
+Installing a Progressive Web App through an install 
+button is only one way users can install a PWA. 
+They can also use Chrome's menu, the mini-infobar,
+ and through an icon in the omnibox. 
+ You can track all of these ways of installation 
+ by listening for the appinstalled event.
+*/
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+  // Clear the deferredPrompt so it can be garbage collected
+  window.deferredPrompt = null;
+});
